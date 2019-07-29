e29f8ed946a90b4d18f3fa88c1eb3e6cdd14b3b8
Mitchell Simoens <mitchellsimoens@gmail.com>
2019-07-29 13:20:45 -0400

    Enable paging

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,10 +8,16 @@ const webpack = require(`webpack`);
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
+  const blogList = path.resolve(`./src/templates/blog-list.tsx`);
   const blogPost = path.resolve(`./src/templates/blog-post.tsx`);
   const result = await graphql(
     `
       {
+        site {
+          siteMetadata {
+            title
+          }
+        }
         allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
           edges {
             node {
@@ -32,8 +38,14 @@ exports.createPages = async ({ graphql, actions }) => {
     throw result.errors;
   }
 
-  // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges;
+  const {
+    data: {
+      allMarkdownRemark: { edges: posts },
+      site: {
+        siteMetadata: { title },
+      },
+    },
+  } = result;
 
   posts.forEach((post, index) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node;
@@ -49,6 +61,24 @@ exports.createPages = async ({ graphql, actions }) => {
       },
     });
   });
+
+  const postsPerPage = 6;
+  const numPages = Math.ceil(posts.length / postsPerPage);
+  Array.from({
+    length: numPages,
+  }).forEach((_, i) =>
+    createPage({
+      path: i === 0 ? `/` : `/page/${i + 1}`,
+      component: blogList,
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+        title,
+      },
+    }),
+  );
 };
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
---
+++ src/templates/blog-list.tsx
@@ -0,0 +1,94 @@
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import Bio from '../components/bio';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { rhythm } from '../utils/typography';
+import './blog-list.css';
+
+interface Props {
+  data: any;
+  location: any;
+  pageContext: any;
+}
+
+const BlogList = (props: Props): JSX.Element => {
+  const {
+    data: {
+      allMarkdownRemark: { edges: posts },
+    },
+    location,
+    pageContext: { currentPage, numPages, title },
+  } = props;
+
+  return (
+    <Layout location={location} title={title}>
+      <SEO title={`Posts Page ${currentPage}`} />
+      <Bio />
+
+      {posts.map(
+        ({ node }: any): JSX.Element => {
+          const postTitle = node.frontmatter.title || node.fields.slug;
+
+          return (
+            <div key={node.fields.slug}>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                  {postTitle}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </div>
+          );
+        },
+      )}
+
+      <div className="blog-list-navigation">
+        {currentPage > 1 && (
+          <Link style={{ boxShadow: `none` }} to={currentPage === 2 ? '' : `/page/${currentPage - 1}`}>
+            {/* <Link style={{ boxShadow: `none` }} to={`/page/${currentPage === 2 ? '' : currentPage - 1}`}> */}← Newer
+            Posts
+          </Link>
+        )}
+        <div className="blog-list-navigation-spacer" />
+        {currentPage < numPages && (
+          <Link style={{ boxShadow: `none` }} to={`/page/${currentPage + 1}`}>
+            Older Posts →
+          </Link>
+        )}
+      </div>
+    </Layout>
+  );
+};
+
+export default BlogList;
+
+export const blogListQuery = graphql`
+  query blogListQuery($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: $limit, skip: $skip) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`;
