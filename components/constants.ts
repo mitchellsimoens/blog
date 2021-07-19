f165945063587cebcfee6f37be840ab5d1eb90ed
Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>
2021-07-19 14:32:06 -0400

    Setup husky

--- components/constants.ts
+++ components/constants.ts
@@ -1,3 +1,4 @@
 export const blogUrl = 'https://mitchellsimoens.com'
-export const imageWorkersUrl = 'https://blog-nextjs-image.mitchellsimoens.workers.dev'
+export const imageWorkersUrl =
+  'https://blog-nextjs-image.mitchellsimoens.workers.dev'
 export const siteTitle = 'Mitchell Simoens Blog'
