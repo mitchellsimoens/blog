19bfc77956e3056f439f211c73ff265e3437eb7f
Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>
2022-01-13 15:57:44 -0500

    Make Image component work with router base path

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from 'react'
 
 import { ImageProps } from 'next/image'
+import { useRouter } from 'next/router'
 
 // const cloudflareImageLoader = ({
 //   quality = 75,
@@ -14,9 +15,11 @@ import { ImageProps } from 'next/image'
 //   return `${imageWorkersUrl}?width=${width}&quality=${quality}&image=${imageUrl.toString()}`
 // }
 
-const Image: FunctionComponent<ImageProps> = (props) => (
-  <img src={props.src as string} alt={props.alt} />
-)
+const Image: FunctionComponent<ImageProps> = (props) => {
+  const router = useRouter()
+
+  return <img src={`${router.basePath}/${props.src}`} alt={props.alt} />
+}
 // process.env.NODE_ENV === 'development' ? (
 //   <NextImage unoptimized={true} {...props} />
 // ) : (
