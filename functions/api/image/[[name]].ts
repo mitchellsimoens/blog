a8d5a954b8f2d2f1a1c7eba12ca564be0557e436
Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>
2022-01-24 15:06:19 -0500

    Add little debugging to image worker
    Signed-off-by: Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>

--- functions/api/image/[[name]].ts
+++ functions/api/image/[[name]].ts
@@ -14,33 +14,37 @@ type EnvVars = {}
 export async function onRequest({
   request,
 }: EventContext<EnvVars, string, void>) {
-  const url = new URL(request.url)
-  const options: Options & RequestInit = {
-    cf: {
-      image: {},
-    },
+  try {
+    const url = new URL(request.url)
+    const options: Options & RequestInit = {
+      cf: {
+        image: {},
+      },
+    }
+
+    if (url.searchParams.has('fit')) {
+      options.cf.image.fit = url.searchParams.get('fit')
+    }
+
+    if (url.searchParams.has('width')) {
+      options.cf.image.width = url.searchParams.get('width')
+    }
+
+    if (url.searchParams.has('height')) {
+      options.cf.image.height = url.searchParams.get('height')
+    }
+
+    if (url.searchParams.has('quality')) {
+      options.cf.image.quality = url.searchParams.get('quality')
+    }
+
+    const imageURL = url.searchParams.get('image')
+    const imageRequest = new Request(imageURL as string, {
+      headers: request.headers,
+    })
+
+    return await fetch(imageRequest, options)
+  } catch (e: any) {
+    return new Response(e.message)
   }
-
-  if (url.searchParams.has('fit')) {
-    options.cf.image.fit = url.searchParams.get('fit')
-  }
-
-  if (url.searchParams.has('width')) {
-    options.cf.image.width = url.searchParams.get('width')
-  }
-
-  if (url.searchParams.has('height')) {
-    options.cf.image.height = url.searchParams.get('height')
-  }
-
-  if (url.searchParams.has('quality')) {
-    options.cf.image.quality = url.searchParams.get('quality')
-  }
-
-  const imageURL = url.searchParams.get('image')
-  const imageRequest = new Request(imageURL as string, {
-    headers: request.headers,
-  })
-
-  return fetch(imageRequest, options)
 }
