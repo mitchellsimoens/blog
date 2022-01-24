f265f072774715993b5a4fa6c936722cccff1de5
Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>
2022-01-24 14:11:30 -0500

    Start of workers
    Signed-off-by: Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>

---
+++ functions/api/image/[[name]].ts
@@ -0,0 +1,46 @@
+interface Options {
+  cf: {
+    image: {
+      fit?: string | null
+      height?: string | null
+      quality?: string | null
+      width?: string | null
+    }
+  }
+}
+
+type EnvVars = {}
+
+export async function onRequest({
+  request,
+}: EventContext<EnvVars, string, void>) {
+  const url = new URL(request.url)
+  const options: Options & RequestInit = {
+    cf: {
+      image: {},
+    },
+  }
+
+  if (url.searchParams.has('fit')) {
+    options.cf.image.fit = url.searchParams.get('fit')
+  }
+
+  if (url.searchParams.has('width')) {
+    options.cf.image.width = url.searchParams.get('width')
+  }
+
+  if (url.searchParams.has('height')) {
+    options.cf.image.height = url.searchParams.get('height')
+  }
+
+  if (url.searchParams.has('quality')) {
+    options.cf.image.quality = url.searchParams.get('quality')
+  }
+
+  const imageURL = url.searchParams.get('image')
+  const imageRequest = new Request(imageURL as string, {
+    headers: request.headers,
+  })
+
+  return fetch(imageRequest, options)
+}
