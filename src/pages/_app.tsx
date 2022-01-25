6e5617446303090466a9020d2345049a9efd319a
Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>
2022-01-25 17:58:30 -0500

    Try importing fa styles directly
    Signed-off-by: Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import { FunctionComponent } from 'react'
 
+import { config } from '@fortawesome/fontawesome-svg-core'
 import { AppProps } from 'next/app'
 
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
 import '../styles/global.css'
+
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
   <Component {...pageProps} />
