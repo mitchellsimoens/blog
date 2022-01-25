c236606de152a1a84d5025605f7743fb1ce01ad2
Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>
2022-01-25 14:29:00 -0500

    Move and use constants in index page

--- src/components/constants.ts
+++
@@ -1,4 +0,0 @@
-export const blogUrl = 'https://mitchellsimoens.com'
-export const imageWorkersUrl =
-  'https://blog-nextjs-image.mitchellsimoens.workers.dev'
-export const siteTitle = 'Mitchell Simoens Blog'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,10 @@
 import Image from '@/components/Image'
+import { siteDescription, siteTitle } from '@/constants'
 import { Meta } from '@/layout/Meta'
 import { Main } from '@/templates/Main'
 
 const Index = () => (
-  <Main
-    meta={
-      <Meta
-        title="Next.js Boilerplate Presentation"
-        description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
-      />
-    }
-  >
+  <Main meta={<Meta title={siteTitle} description={siteDescription} />}>
     <a href="https://github.com/ixartz/Next-js-Boilerplate">
       <Image
         src={`/assets/images/nextjs-starter-banner.png`}
