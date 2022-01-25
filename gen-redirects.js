d3c5b1e003d63d18f31965b122be94d7934040e9
Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>
2022-01-25 13:44:32 -0500

    Add redirects script
    Signed-off-by: Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>

---
+++ gen-redirects.js
@@ -0,0 +1,47 @@
+;(async () => {
+  const { writeFile } = await import('fs/promises')
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  const { globbySync } = await import('globby')
+  const { dirname } = await import('path')
+  const files = globbySync('content/**/*')
+  const parsed = files
+    .map((file) => {
+      const parsedFile = file.replace('content', '')
+      const dest = dirname(parsedFile)
+      const from = dest.replace('/blog', '')
+      const [, year] = from.split('/')
+
+      // 2021 is the year moving to cloudflare pages
+      // so we don't need to create redirects for blogs
+      // after. This keeps us under 100 redirects which
+      // is the limit
+      if (parseInt(year, 10) < 2021) {
+        return {
+          from,
+          dest,
+        }
+      }
+
+      return null
+    })
+    .filter(Boolean)
+
+  // remove duplicates that can happen from multiple
+  // blogs in a day or if a blog has other files in
+  // the same dir
+  const unique = parsed.reduce((filter, current) => {
+    const match = filter.find((item) => item.from === current.from)
+
+    if (match) {
+      return filter
+    }
+
+    return filter.concat([current])
+  }, [])
+
+  const formatted = unique
+    .map(({ from, dest }) => `${from} ${dest} 301`)
+    .join('\n')
+
+  await writeFile(`./_redirects`, formatted)
+})()
