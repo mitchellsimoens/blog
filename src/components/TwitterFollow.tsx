1794191fb2848277aa7751303b13639f9d5bfe1c
Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>
2022-02-16 16:29:30 -0500

    Add twitter follow component (#38)
    * Add twitter follow component
    
    * Add twitter to CSP header
    
    * Make twitter csp less strict
    
    * Add to default-src csp
    
    * Add even more twitter csp

--- src/components/TwitterFollow.tsx
+++ src/components/TwitterFollow.tsx
@@ -1,17 +1,14 @@
 import { FunctionComponent } from 'react'
 
-// https://github.com/saurabhnemade/react-twitter-embed/pull/89 is blocking this, doesn't work with React version
-// import { TwitterFollowButton } from 'react-twitter-embed'
+import { TwitterFollowButton } from 'react-twitter-embed'
 
 interface Props {
   handle: string
   size?: string
 }
 
-// const Author: FunctionComponent<Props> = ({ handle, size = 'large' }) => (
-//   <TwitterFollowButton options={{ size }} screenName={handle} />
-// )
-
-const Author: FunctionComponent<Props> = () => null
+const Author: FunctionComponent<Props> = ({ handle, size = 'large' }) => (
+  <TwitterFollowButton options={{ size }} screenName={handle} />
+)
 
 export default Author
